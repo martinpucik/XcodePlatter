e5be6ffb2335a717dd1491a021867d3b5e65dae3
Martin Pucik <martin.pucik@me.com>
2018-02-02 15:48:33 +0100

    [Added] UIView Template

---
+++ templates/UIView.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,36 @@
+//
+// ___FILENAME___
+// Created by ___FULLUSERNAME___ on ___DATE___
+// ___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIView {
+    // MARK: - Public Props
+
+    // MARK: - Private Props
+
+    // MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+        setupConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) { return nil }
+
+    // MARK: - Public Interface
+    
+}
+
+// MARK: - Private Methods
+private extension ___FILEBASENAMEASIDENTIFIER___ {
+    func setupLayout() {
+
+    }
+
+    func setupConstraints() {
+
+    }
+}
