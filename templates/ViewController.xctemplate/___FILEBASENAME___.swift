1c441145264c8b8b630044c7c83d4111df51ad0e
Martin Pucik <martin.pucik@me.com>
2018-02-15 15:58:05 +0100

    [Added] ViewController Template

---
+++ templates/ViewController.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,37 @@
+//
+// ___FILENAME___
+// Created by ___FULLUSERNAME___ on ___DATE___
+// ___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    
+    // MARK: - Public Props
+
+    // MARK: - Private Props
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+        setupConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) { return nil }
+
+    // MARK: - Public Interface
+    
+}
+
+// MARK: - Private Methods
+private extension ___FILEBASENAMEASIDENTIFIER___ {
+    func setupLayout() {
+
+    }
+
+    func setupConstraints() {
+
+    }
+}
